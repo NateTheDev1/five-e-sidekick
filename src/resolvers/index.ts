504491a7f4cc70250887986bd82245c8d0b218c3
Nathaniel Richards <nathaniel@datacompusa.com>
2021-06-07 17:48:41 -0400

    Exporting resolvers now

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,4 +1,13 @@
+import { login } from './mutation/user/login';
+import { signup } from './mutation/user/signup';
+import { getUser } from './query/user/getUser';
+
 export const resolvers: Resolvers.Resolvers = {
-	Query: <Resolvers.QueryResolvers>{},
-	Mutation: <Resolvers.MutationResolvers>{}
+	Query: <Resolvers.QueryResolvers>{
+		getUser: getUser
+	},
+	Mutation: <Resolvers.MutationResolvers>{
+		login: login,
+		signup: signup
+	}
 };
